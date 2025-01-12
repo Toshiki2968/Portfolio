867282bffdbde29cc1627cb8a556ec4ab387bed1
t-sumiya-uf <tsk2968@gmail.com>
2025-01-12 15:37:46 +0900

    add: レイアウト修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,12 @@ module.exports = {
         "selected-text": "#059ebf",
         theme: "#5c318c",
         secondary: "#919aA4",
-        badge: "#3F3F51",
+        badge: "#e3f6f9",
+        "badge-text": "#697477",
         inputBorder: "#565666",
         input: "#2A2A33",
+        "secondary-title": "#2a3b47",
+        title: "#2a3b47",
       },
       fontFamily: {
         poppins: ["Poppins", "serif"],
